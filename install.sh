5ae818194285cf9eb9d50d9af62e3ec1554feb69
Krzysztof Magosa <krzysztof@magosa.pl>
2015-03-07 23:44:03 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+function make_symlinks()
+{
+    cd "$(dirname "${BASH_SOURCE}")"
+    path=$(pwd)
+
+    for name in .* ; do
+        if [[ ! "$name" =~ ~$ && "$name" != "." && "$name" != ".." ]] ; then
+            echo "Linking $name..."
+            ln -sf "$path/$name" ~/
+        fi
+    done
+}
+
+echo "All files/directories beggining with . in this repository"
+echo "will be symlinked to your home directory, existing symlinks"
+echo "may be overwritten, are you sure to continue?"
+
+select yn in "yes" "no";
+do
+    case $yn in
+        yes) make_symlinks; break;;
+        no) exit;;
+    esac
+done
