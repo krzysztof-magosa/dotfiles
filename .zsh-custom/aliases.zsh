8abef85ac0b70e1f44326f0bc7ca598022c8b7cb
Krzysztof Magosa <krzysztof@magosa.pl>
2015-03-08 13:26:26 +0100

    Add some aliases

---
+++ .zsh-custom/aliases.zsh
@@ -0,0 +1,7 @@
+alias tigs="tig status"
+
+# Show external IP
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+# Lock the screen
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
