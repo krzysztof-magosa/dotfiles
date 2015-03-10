86d108d65d7f56fba3b8328bfd9a10ee9b8331e4
Krzysztof Magosa <krzysztof@magosa.pl>
2015-03-10 19:43:05 +0100

    Ability to install without asking

--- install.sh
+++ install.sh
@@ -13,14 +13,19 @@ function make_symlinks()
     done
 }
 
-echo "All files/directories beggining with . in this repository"
-echo "will be symlinked to your home directory, existing symlinks"
-echo "may be overwritten, are you sure to continue?"
+if [ "$1" = "--force" ] ; then
+    make_symlinks
+else
+    echo "All files/directories beggining with . in this repository"
+    echo "will be symlinked to your home directory, existing symlinks"
+    echo "may be overwritten, are you sure to continue?"
+    
+    select yn in "yes" "no";
+    do
+        case $yn in
+            yes) make_symlinks; break;;
+            no) exit;;
+        esac
+    done
+fi
 
-select yn in "yes" "no";
-do
-    case $yn in
-        yes) make_symlinks; break;;
-        no) exit;;
-    esac
-done
