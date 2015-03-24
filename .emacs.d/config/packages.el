86c593e2750a51915c3e4c5ad5c9096aae55133c
Krzysztof Magosa <krzysztof@magosa.pl>
2015-03-24 17:43:56 +0100

    git gutter for programming modes

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -168,6 +168,18 @@
   :ensure t
   :mode "\\.json\\'")
 
+(use-package git-gutter
+  :ensure t
+  :defer t
+  :commands git-gutter-mode
+  :init
+  (progn
+    (add-hook 'prog-mode-hook 'git-gutter-mode))
+  :config
+  (progn
+    (git-gutter:linum-setup)
+    (setq git-gutter:update-interval 2)))
+
 (use-package elpy
   :ensure t
   :defer t
