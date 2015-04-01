5a44310dcf4249ed96befdbd3a628183a5f4282d
Krzysztof Magosa <krzysztof@magosa.pl>
2015-04-01 13:38:59 +0200

    Configure projectile

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -223,6 +223,19 @@
 (use-package osx-clipboard
   :ensure t)
 
+(use-package projectile
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (add-hook 'prog-mode-hook 'projectile-mode))
+  :config
+  (progn
+    (setq projectile-project-root-files '(".project-root"))
+    (setq projectile-project-root-files-top-down-recurring '(".project-root"))
+    (setq projectile-project-root-files-bottom-up '(".project-root"))
+    (setq projectile-enable-idle-timer t)))
+
 ; it must be here on the bottom, because of bug with cl lib
 (use-package fiplr
   :ensure t
