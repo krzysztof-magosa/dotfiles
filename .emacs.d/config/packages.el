1a3c2cdc0b8f93af0ce66267cd5893be9c27fc10
Krzysztof Magosa <krzysztof@magosa.pl>
2015-05-08 13:52:34 +0200

    show also files ignored by git

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -275,6 +275,7 @@
     (setq projectile-project-root-files '(".project-root"))
     (setq projectile-project-root-files-top-down-recurring '(".project-root"))
     (setq projectile-project-root-files-bottom-up '(".project-root"))
+    (setq projectile-git-command "git ls-files -zco")
     (setq projectile-completion-system 'helm)
     (setq projectile-enable-idle-timer t)))
 
