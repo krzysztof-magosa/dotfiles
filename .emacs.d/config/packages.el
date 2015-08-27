8b51957f4464f7e12a3c94f119d0759451b2c854
Krzysztof Magosa <krzysztof@magosa.pl>
2015-08-27 14:45:46 +0200

    use yasnippet for shell scripts

--- .emacs.d/config/packages.el
+++ .emacs.d/config/packages.el
@@ -94,6 +94,7 @@
     (setq yas-snippet-dirs "~/.emacs.d/snippets")
     (add-hook 'c-mode-hook 'yas-global-mode)
     (add-hook 'c++-mode-hook 'yas-global-mode)
+    (add-hook 'sh-mode-hook 'yas-global-mode)
     (add-hook 'php-mode-hook 'yas-global-mode))) ; just for php
 
 ; php
