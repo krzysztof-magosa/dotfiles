4cc3f23710393a94252542ab2dceef7a15391a34
Krzysztof Magosa <krzysztof@magosa.pl>
2015-08-27 13:43:51 +0200

    handy alias for debugging akamai

--- .zsh-custom/aliases.zsh
+++ .zsh-custom/aliases.zsh
@@ -2,6 +2,7 @@ alias tigs="tig status"
 alias mkdir="mkdir -p"
 alias e='$EDITOR'
 alias n="cd ~/.notes"
+alias akamai="curl -H 'Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no' -IXGET "
 
 if [ -d ~/.dotvm ] ; then
    alias vm="(cd ~/.dotvm && vagrant ssh)"
