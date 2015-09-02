4810f29593e296f69e3dc156f6ac1c832eb62852
Krzysztof Magosa <krzysztof@magosa.pl>
2015-09-02 15:22:36 +0200

    diminish some always used minor modes

--- .emacs.d/init/init-appearance.el
+++ .emacs.d/init/init-appearance.el
@@ -30,4 +30,15 @@
   (progn
     (powerline-default-theme)))
 
+(use-package diminish
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (eval-after-load "projectile" '(diminish 'projectile-mode))
+    (eval-after-load "osx-clipboard" '(diminish 'osx-clipboard-mode))
+    (eval-after-load "git-gutter" '(diminish 'git-gutter-mode))
+    (eval-after-load "highlight-current-line" '(diminish 'highlight-current-line-minor-mode))
+    (eval-after-load "company" '(diminish 'company-mode))))
+
 (provide 'init-appearance)
