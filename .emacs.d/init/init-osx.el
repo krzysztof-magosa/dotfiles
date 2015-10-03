71d696b5e6be54bf6fd7f0118e1d9f91354d2c1b
Krzysztof Magosa <krzysztof@magosa.pl>
2015-10-03 13:23:13 +0200

    remove .emacs.d, will be stored in separate repo

--- .emacs.d/init/init-osx.el
+++
@@ -1,7 +0,0 @@
-(use-package osx-clipboard
-  :ensure t
-  :init
-  (progn
-    (osx-clipboard-mode)))
-
-(provide 'init-osx)
--- .emacs.d/init/init-ruby.el
+++
@@ -1,9 +0,0 @@
-(use-package ruby-additional
-  :ensure t
-  :defer t)
-
-(use-package rubocop
-  :ensure t
-  :defer t)
-
-(provide 'init-ruby)
