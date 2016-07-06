4bb7360046a885a88034b46b7ed190d2330ca252
Krzysztof Magosa <krzysztof.magosa@performgroup.com>
2016-07-06 17:39:34 +0200

    alias to notes dir

--- .zsh/conf.d/900.aliases.zsh
+++ .zsh/conf.d/900.aliases.zsh
@@ -6,6 +6,7 @@ alias tigs="tig status"
 alias mkdir="mkdir -p"
 alias e='$EDITOR'
 alias n="emacs '$ICLOUD_DIR/notes.txt'"
+alias nn="mkdir '$ICLOUD_DIR/Notes' ; cd '$ICLOUD_DIR/Notes'"
 alias akamai="curl -H 'Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no' -IXGET "
 
 if [ -d ~/.dotvm ] ; then
