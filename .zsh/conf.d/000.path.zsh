96ec17f4c5bc41d9e67d82228643466e742557b9
Krzysztof Magosa <krzysztof@magosa.pl>
2016-07-10 13:39:03 +0200

    additional paths

--- .zsh/conf.d/000.path.zsh
+++ .zsh/conf.d/000.path.zsh
@@ -2,8 +2,11 @@ path=(
     /opt/homebrew/bin
     /opt/homebrew/sbin
     ~/bin
+    ~/bin/haxor/bin
     ~/.bin/tig-rebase
     ~/Library/Python/2.7/bin
     ~/.rbenv/bi
+    $(ruby -rubygems -e 'puts Gem.user_dir')/bin
+    ~/Library/Python/2.7/bin
     $path
 )
