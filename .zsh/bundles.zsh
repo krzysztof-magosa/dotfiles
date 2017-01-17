d5fb96bee472e0b6d9a06386fb6220b478a4b943
KM <krzysztof@magosa.pl>
2017-01-17 21:34:49 +0100

    maybe will use it at some point but not now

--- .zsh/bundles.zsh
+++ .zsh/bundles.zsh
@@ -7,20 +7,20 @@ zplug "plugins/brew-cask", from:oh-my-zsh, defer:2
 zplug "plugins/copydir", from:oh-my-zsh
 zplug "plugins/copyfile", from:oh-my-zsh
 zplug "plugins/cp", from:oh-my-zsh # cpv
-zplug "plugins/virtualenv", from:oh-my-zsh
+#zplug "plugins/virtualenv", from:oh-my-zsh
 zplug "plugins/vagrant", from:oh-my-zsh
 zplug "plugins/sudo", from:oh-my-zsh # esc-esc
-zplug "plugins/rbenv", from:oh-my-zsh
+#zplug "plugins/rbenv", from:oh-my-zsh
 zplug "plugins/python", from:oh-my-zsh
 zplug "plugins/forklift", from:oh-my-zsh # fl <enter>
 zplug "plugins/history", from:oh-my-zsh # hs/hsi
 zplug "plugins/osx", from:oh-my-zsh, defer:2
 
 # various stuff
-zplug "fcambus/ansiweather"
+#zplug "fcambus/ansiweather"
 zplug "mollifier/cd-gitroot"
 zplug "voronkovich/gitignore.plugin.zsh", defer:2 # gi <lang>
-zplug "peterhurford/git-it-on.zsh" # gitit
+#zplug "peterhurford/git-it-on.zsh" # gitit
 zplug "Seinh/git-prune" # gprune
 zplug "supercrabtree/k" # k
 zplug "junegunn/fzf", use:"shell/*.zsh", defer:2 # fuzzy search, ctrl+t, ctrl+r, alt+c
