afc1288bbc1636a358c8a8872049f3d849f59e22
Krzysztof Magosa <krzysztof@magosa.pl>
2017-04-01 19:19:37 +0200

    don't complain when directory is empty

--- .zsh/900.completion.zsh
+++ .zsh/900.completion.zsh
@@ -2,6 +2,6 @@ hosts_dir="$ZSH_CUSTOM/hosts"
 
 if [ -d $hosts_dir ] ; then
     zstyle -s ':completion:*:hosts' hosts _completion_hosts
-    _completion_hosts+=($(cat $hosts_dir/*))
+    _completion_hosts+=($(find $hosts_dir -type f -exec cat {} \;))
     zstyle ':completion:*:hosts' hosts $_completion_hosts
 fi
