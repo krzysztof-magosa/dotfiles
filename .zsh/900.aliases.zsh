177282822db4ffb18001b81a6be96aa57ab71c12
Krzysztof Magosa <krzysztof@magosa.pl>
2017-04-05 08:38:02 +0200

    short alias to kill emacs

--- .zsh/900.aliases.zsh
+++ .zsh/900.aliases.zsh
@@ -8,6 +8,7 @@ alias mkdir="mkdir -p"
 alias e='emacsclient -a "" -c -nw'
 alias e.='e .'
 alias emacs='emacs -nw'
+alias ek='killall Emacs emacs'
 alias n="e '$ICLOUD_DIR/notes.txt'"
 alias nn="mkdir '$ICLOUD_DIR/Notes' ; cd '$ICLOUD_DIR/Notes'"
 alias akamai="curl -H 'Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no' -IXGET "
