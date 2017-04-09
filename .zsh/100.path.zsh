75f09abab62d05b6e568184bae24094fbd9f6f80
Krzysztof Magosa <krzysztof@magosa.pl>
2017-04-09 13:15:18 +0200

    path to diff highlight

--- .zsh/100.path.zsh
+++ .zsh/100.path.zsh
@@ -4,6 +4,7 @@ export PYTHONUSERBASE=~/.python
 path=(
     /opt/homebrew/bin
     /opt/homebrew/sbin
+    /opt/homebrew/share/git-core/contrib/diff-highlight
     ~/.python/bin
     ~/.npm/bin
     ~/.go/bin
