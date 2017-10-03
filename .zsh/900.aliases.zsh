b179be750c73076f0b63c72052e86eac202cc1fc
Krzysztof Magosa <krzysztof@magosa.pl>
2017-10-03 23:08:31 +0200

    high sierra provides dedicated shortcut for that

--- .zsh/900.aliases.zsh
+++ .zsh/900.aliases.zsh
@@ -34,6 +34,3 @@ fi
 
 # Show external IP
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-
-# Lock the screen
-is_osx && alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
