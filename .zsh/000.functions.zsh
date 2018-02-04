61a1bd256bea5c1ebf31849811cd337e20b108a9
Krzysztof Magosa <krzysztof@magosa.pl>
2018-02-04 11:40:29 +0100

    alias for printing alias hosts

--- .zsh/000.functions.zsh
+++ .zsh/000.functions.zsh
@@ -6,3 +6,18 @@ function is_linux() {
          [[ "$OSTYPE" =~ ^linux ]] || return 1
 }
 
+function ansible_hosts() {
+    pattern=$1
+    mode=$2
+
+    for core in host_vars/$~pattern/core.yml ; do
+        host=$(basename $(dirname $core))
+        ip=$(grep ansible_host: $core | awk '{print $NF}' | sed 's/"//g' | sed "s/'//g")
+
+        if [ "$mode" = "a" ] ; then
+            echo "$host A $ip"
+        else
+            echo "$host has IP $ip"
+        fi
+    done
+}
