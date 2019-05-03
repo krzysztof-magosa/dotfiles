84d991679ab9a096ca382819b178ab9e0396536e
Krzysztof Magosa <krzysztof@magosa.pl>
2019-05-04 00:08:01 +0200

    move git related aliases into once place

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,10 +1,8 @@
 alias today='git log --since="today 00:00:00" --author="$(git config user.email)" --format=oneline | egrep "\w+ \w+-\d+" | cut -d" " -f 2-'
 alias yesterday='git log --since="yesterday 00:00:00" --before="today 00:00:00" --author="$(git config user.email)" --format=oneline | egrep "\w+ \w+-\d+" | cut -d" " -f 2-'
 
-alias g="git"
 alias cd..="cd .."
 alias mc="mc -u"
-alias tigs="tig status"
 alias mkdir="mkdir -p"
 alias akamai="curl -H 'Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no' -IXGET "
 alias avv="$HOME/.bin/ansible-inline-vault-view"
@@ -12,12 +10,16 @@ alias ave="ansible-vault-encrypt-oneline-string"
 alias avec="ansible-vault-encrypt-oneline-string | pbcopy"
 alias avd="sed -E 's/^[ \t]+//g' | ansible-vault decrypt 2>/dev/null"
 alias aves="ansible-vault encrypt_string"
-alias hpr="hub pull-request"
-alias grb="comm -13 <(git branch -r | sed 's@origin/@@'g | awk '{print \$1}' | sort) <(git branch -l | sed 's/*//g' |  awk '{print \$1}')"
 
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+
+# Git
+alias g="git"
+alias grb="comm -13 <(git branch -r | sed 's@origin/@@'g | awk '{print \$1}' | sort) <(git branch -l | sed 's/*//g' |  awk '{print \$1}')"
+alias hpr="hub pull-request"
+alias tigs="tig status"
 
 # Handy alias to copy public key
 if  [ -f ~/.ssh/id_rsa.pub ] && is_osx ; then
