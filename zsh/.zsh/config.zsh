187000d059b444bed3bd00e09e60eb9d59d03150
Krzysztof Magosa <krzysztof@magosa.pl>
2019-05-04 09:47:53 +0200

    reconfigure zsh options

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -1,15 +1,21 @@
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=1000
+SAVEHIST=100000
 
+# Change directory without cd
 setopt AUTO_CD
-setopt INC_APPEND_HISTORY
+
+# Share history between sessions
 setopt SHARE_HISTORY
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_REDUCE_BLANKS
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_SPACE
-setopt HIST_VERIFY
+
+# Expire duplicates before unique commands
 setopt HIST_EXPIRE_DUPS_FIRST
 
+# Remove superfluous blanks from each command line being added to the history list.
+setopt HIST_REDUCE_BLANKS
 
+# Do not remember commands that start with a whitespace
+setopt HIST_IGNORE_SPACE
+
+# Do not run command by !! without verification
+setopt HIST_VERIFY
