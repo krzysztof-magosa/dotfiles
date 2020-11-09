b99eb7ac054df95c7cf5dc6950b0e8e83ced1534
Krzysztof Magosa <krzysztof@magosa.pl>
2020-11-10 00:07:53 +0100

    remove non functioning block

--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -1,9 +1,3 @@
-# Setup fzf
-# ---------
-  export PATH="$PATH:/opt/homebrew/opt/fzf/bin"
-if [[ ! "$PATH" == *$HOME/.brew/opt/fzf/bin* ]]; then
-fi
-
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "$HOME/.brew/opt/fzf/shell/completion.zsh" 2> /dev/null
