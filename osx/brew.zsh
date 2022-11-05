82729af3b6f6b559f9f4722f6d1ea9c5cbf7135c
Krzysztof Magosa <krzysztof@magosa.pl>
2022-11-05 12:03:10 +0100

    store required apps

---
+++ osx/brew.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+brew install \
+    fzf \
+    git \
+    htop \
+    midnight-commander \
+    neovim \
+    stow \
+    tig \
+    dockutil \
+    font-hack \
+    iterm2 \
+    vlc
