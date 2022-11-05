68150e9d15aacbe9a48786aa5b2367bdea04fa21
Krzysztof Magosa <krzysztof@magosa.pl>
2022-11-05 12:05:59 +0100

    add notes to dock

--- osx/dock.zsh
+++ osx/dock.zsh
@@ -4,6 +4,7 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/System/Applications/Reminders.app"
+dockutil --no-restart --add "/System/Applications/Notes.app"
 dockutil --no-restart --add "/Applications/Enpass.app"
 dockutil --no-restart --add "/System/Applications/System Settings.app"
 
