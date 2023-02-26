9fb9c75782344e5f22b6d515f912618b3352ce10
Krzysztof Magosa <krzysztof.magosa@statsperform.com>
2023-02-26 18:17:15 +0100

    use safari

--- osx/dock.zsh
+++ osx/dock.zsh
@@ -9,7 +9,7 @@ function add_icon() {
 
 
 dockutil --no-restart --remove all
-add_icon "/Applications/Microsoft Edge.app"
+add_icon "/Applications/Safari.app"
 add_icon "/Applications/iTerm.app"
 add_icon "/System/Applications/Reminders.app"
 add_icon "/System/Applications/Calendar.app"
