dfdd08a3030a83659992bec4d7a9347e021a37be
Krzysztof Magosa <krzysztof.magosa@statsperform.com>
2025-06-17 10:03:29 +0200

    update dock

--- osx/dock.zsh
+++ osx/dock.zsh
@@ -9,15 +9,16 @@ function add_icon() {
 
 
 dockutil --no-restart --remove all
-add_icon $(readlink -f "/Applications/Safari.app")
 add_icon "/Applications/iTerm.app"
-add_icon "/System/Applications/Reminders.app"
-add_icon "/System/Applications/Calendar.app"
+add_icon "/Applications/Microsoft Edge.app"
+add_icon "/Applications/Visual Studio Code.app"
 add_icon "/System/Applications/Notes.app"
 add_icon "/Applications/Microsoft Outlook.app"
 add_icon "/Applications/Microsoft Teams.app"
-add_icon "/Applications/Enpass.app"
 add_icon "/System/Applications/System Settings.app"
+add_icon "/Applications/Enpass.app"
+add_icon "/System/Applications/Calendar.app"
+add_icon "$HOME/Downloads"
 
 killall Dock
 
