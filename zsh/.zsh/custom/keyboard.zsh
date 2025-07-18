8e808530a2e5eafac9e89f017060427fb14e90be
Krzysztof Magosa <krzysztof.magosa@statsperform.com>
2025-07-18 14:34:58 +0200

    this seems colliding with remap on ghostty level

--- zsh/.zsh/custom/keyboard.zsh
+++ zsh/.zsh/custom/keyboard.zsh
@@ -1,8 +1,8 @@
 # fix home/end on os x
 bindkey '^[[H' beginning-of-line
 bindkey '^[[F' end-of-line
-bindkey '^[[1~' beginning-of-line
-bindkey '^[[4~' end-of-line
+#bindkey '^[[1~' beginning-of-line
+#bindkey '^[[4~' end-of-line
 
 bindkey "^[f" forward-word
 bindkey "^[b" backward-word
