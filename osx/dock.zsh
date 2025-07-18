c9039f7bc08c466cc2820d54215285160b5b5ff8
Krzysztof Magosa <krzysztof@magosa.pl>
2025-07-18 20:08:44 +0200

    update apps in dock

--- osx/dock.zsh
+++ osx/dock.zsh
@@ -10,14 +10,15 @@ function add_icon() {
 
 dockutil --no-restart --remove all
 add_icon "/Applications/Ghostty.app"
-add_icon "/Applications/Microsoft Edge.app"
 add_icon "/Applications/Visual Studio Code.app"
+add_icon "/System/Cryptexes/App/System/Applications/Safari.app"
 add_icon "/System/Applications/Notes.app"
 add_icon "/Applications/Microsoft Outlook.app"
 add_icon "/Applications/Microsoft Teams.app"
-add_icon "/System/Applications/System Settings.app"
-add_icon "/Applications/Enpass.app"
+add_icon "/Applications/1Password.app"
+add_icon "/Applications/Spotify.app"
 add_icon "/System/Applications/Calendar.app"
+add_icon "/System/Applications/System Settings.app"
 add_icon "$HOME/Downloads"
 
 killall Dock
