1fb68929b8569a8876abc343fbef898f05569657
Krzysztof Magosa <krzysztof@magosa.pl>
2025-07-26 14:03:55 +0200

    shorter name

--- bin/.bin/bstrap.sh
+++ bin/.bin/bstrap.sh
@@ -4,3 +4,5 @@ open -g "rectangle-pro://execute-layout?name=Work"
 open -g "rectangle-pro://execute-layout?name=Comms"
 open -g "rectangle-pro://execute-layout?name=Media"
 open -g "rectangle-pro://execute-layout?name=Other"
+
+/Applications/FlashSpace.app/Contents/Resources/flashspace workspace --number 1
