ea985bddf17a63c394965f6fa9681c15160dd88d
Krzysztof Magosa <krzysztof@magosa.pl>
2025-07-27 15:11:40 +0200

    give some time for apps to start

--- bin/.bin/bstrap.sh
+++ bin/.bin/bstrap.sh
@@ -5,4 +5,6 @@ open -g "rectangle-pro://execute-layout?name=Comms"
 open -g "rectangle-pro://execute-layout?name=Media"
 open -g "rectangle-pro://execute-layout?name=Other"
 
+sleep 5
+
 /Applications/FlashSpace.app/Contents/Resources/flashspace workspace --number 1
